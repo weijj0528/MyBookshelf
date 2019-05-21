39d74e2da8eb702408267268f3270bde60115e24
weiun <weijj0528@163.com>
2019-05-22 00:08:43 +0800

    自用调整

--- app/src/main/java/com/kunfei/bookshelf/model/content/BookContent.java
+++ app/src/main/java/com/kunfei/bookshelf/model/content/BookContent.java
@@ -60,11 +60,11 @@ class BookContent {
             if (TextUtils.isEmpty(baseUrl)) {
                 baseUrl = NetworkUtils.getAbsoluteURL(bookShelfBean.getBookInfoBean().getChapterUrl(), chapterBean.getDurChapterUrl());
             }
-            if (StringUtils.isJsonType(s) && !MApplication.getInstance().getDonateHb()) {
+            /*if (StringUtils.isJsonType(s) && !MApplication.getInstance().getDonateHb()) {
                 e.onError(new Throwable(MApplication.getInstance().getString(R.string.donate_s)));
                 e.onComplete();
                 return;
-            }
+            }*/
             Debug.printLog(tag, "┌成功获取正文页");
             Debug.printLog(tag, "└" + baseUrl);
             BookContentBean bookContentBean = new BookContentBean();
